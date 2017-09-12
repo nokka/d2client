f031ab4eb2a07eee8bfe5eae18bb04878b575e08
Stefan Konno <stefan.konno@schibsted.se>
2017-09-12 19:13:51 +0200

    Initial commit.

---
+++ README.md
@@ -0,0 +1,63 @@
+# Diablo II TCP client
+[![Go Report Card](https://goreportcard.com/badge/github.com/nokka/d2client)](https://goreportcard.com/report/github.com/nokka/d2client)
+[![GoDoc](https://godoc.org/github.com/nokka/d2client?status.svg)](https://godoc.org/github.com/nokka/d2client)
+
+D2Client is a TCP client that logs into the [Diablo II](http://eu.blizzard.com/en-gb/games/d2/)  server and can then write messages to the server, both in the chat and whisper specific accounts.
+
+
+## Install
+
+```bash
+$ go get github.com/nokka/d2client
+```
+
+## Usage
+
+```go
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/nokka/d2client"
+)
+
+func main() {
+	client := d2client.New()
+	client.Open("private.server:3001")
+	defer client.Close()
+
+	// Setup channel to read on.
+	ch := make(chan []byte)
+
+	// Setup output error channel.
+	errors := make(chan error)
+
+	client.Read(ch, errors)
+
+	err := client.Login("user", "password")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	client.Whisper("nokka", "Hello!")
+
+
+	// Read the output from the chat onto a channel.
+	for {
+		select {
+		// This case means we recieved data on the connection.
+		case data := <-ch:
+			fmt.Println(string(data))
+
+		case err := <-errors:
+			fmt.Println(err)
+			break
+		}
+	}
+}
+
+## Contributing
+
+Please see [CONTRIBUTING.md](/CONTRIBUTING.md).
---
+++ client.go
@@ -0,0 +1,148 @@
+package d2client
+
+import (
+	"errors"
+	"fmt"
+	"net"
+)
+
+var (
+	// ErrUnableToWrite is used when we try to write on the connection and fail.
+	ErrUnableToWrite = errors.New("Unable to write over the connection")
+
+	// ErrNotConnected will be used when a write is being performed on a closed connection.
+	ErrNotConnected = errors.New("Connection has not been opened")
+)
+
+// Client provides operations on the diablo game client.
+type Client interface {
+	// Opens a connection to the given host.
+	Open(string) error
+
+	// Closes the connection.
+	Close()
+
+	// Logs the given user onto the host.
+	Login(string, string) error
+
+	// Write will send a message over TCP to the diablo server.
+	Write(string) error
+
+	// Whisper is a helper function to send a message directly to an account.
+	Whisper(string, string) error
+
+	// Reads all the data over the TCP connection.
+	Read(chan []byte, chan error)
+}
+
+type client struct {
+	connection net.Conn
+}
+
+func (c *client) Open(host string) error {
+	// Resolve the host.
+	addr, err := net.ResolveTCPAddr("tcp", host)
+	if err != nil {
+		return nil
+	}
+
+	// Dial the host for a connection.
+	conn, err := net.DialTCP("tcp", nil, addr)
+	if err != nil {
+		return nil
+	}
+
+	// Set connection on the client.
+	c.connection = conn
+
+	return nil
+}
+
+// Close will close the socket if it's open.
+func (c *client) Close() {
+	if c.connection != nil {
+		c.connection.Close()
+	}
+}
+
+// Login needs to run before you can actually send any message, will
+// return an error if we can't connect to the server.
+func (c *client) Login(account string, password string) error {
+	if c.connection == nil {
+		return ErrNotConnected
+	}
+
+	// Write First carriage return.
+	_, err := c.connection.Write([]byte("\r\n"))
+	if err != nil {
+		return err
+	}
+
+	// Write account name.
+	accountMsg := fmt.Sprintf("%s\n\n", account)
+	_, err = c.connection.Write([]byte(accountMsg))
+	if err != nil {
+		return err
+	}
+
+	// Write password.
+	passwordMsg := fmt.Sprintf("%s\n\n", password)
+	_, err = c.connection.Write([]byte(passwordMsg))
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Write will take the message and write it over the connection.
+func (c *client) Write(message string) error {
+	msg := fmt.Sprintf("%s\n\n", message)
+
+	// Write message.
+	_, err := c.connection.Write([]byte(msg))
+	if err != nil {
+		return ErrUnableToWrite
+	}
+
+	return nil
+}
+
+// Whisper is a helper function to whisper a specific account with a message.
+func (c *client) Whisper(account string, message string) error {
+	msg := fmt.Sprintf("/msg %s %s\n\n", account, message)
+
+	// Whisper message to the account.
+	_, err := c.connection.Write([]byte(msg))
+	if err != nil {
+		return ErrUnableToWrite
+	}
+
+	return nil
+}
+
+// Reads all the output of the tcp connection on a channel.
+func (c *client) Read(ch chan []byte, errors chan error) {
+
+	// Make a byte slice to read data into.
+	buf := make([]byte, 1024)
+
+	go func(ch chan []byte, errs chan error) {
+		for {
+			// Read the amount of bytes into the buf.
+			bytes, err := c.connection.Read(buf)
+			if err != nil {
+				errs <- err
+				return
+			}
+
+			// Only send the newly written bytes to the channel.
+			ch <- buf[:bytes]
+		}
+	}(ch, errors)
+}
+
+// New creates a new client with all dependencies.
+func New() Client {
+	return &client{}
+}
